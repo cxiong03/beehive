c8795810a4c3e2abc28a4072eeedf9f04e507700
Chang <cnxiong03@gmail.com>
2020-04-10 15:45:03 -0400

    created Resource file in component folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component} from 'react';
 import './App.css';
+import Resource from './components/Resource';
 
 class App extends Component {
+
+  resource = {
+    posterName: 'Cliff',
+    resourceAuthor: 'NetNinja',
+    authorSkillLevel: 'Advanced',
+    cohort: '0',
+    title: 'React & Redux Complete',
+    categories: ['React', 'Redux'],
+    summary: 'A good resource on building a couple react apps with and without redux',
+    link: 'www.google.com/search?q=netNinja%20React',
+    resourceType: 'Playlist', 
+    datePublished: '2018-09-10.00:00:00.000z',
+    videoLength: null,
+    timeToComplete: 300,
+    rating: 5,
+    comments: [{user: 'Joe', text:'Pretty good resource'}, {user: 'Phen', text: 'The best to learn React with'}]
+}
 
   render() {
     return (
       <div className="App">
         <h1>Welcome to BrainHive!</h1>
+        <Resource resource={this.resource} />
+        <Resource resource={this.resource} />
+        <Resource resource={this.resource} />
       </div>
     );
   }
