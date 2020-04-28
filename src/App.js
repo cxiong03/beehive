545ec9ed31c40f083e7cece65d23f08974375827
Chang <cnxiong03@gmail.com>
2020-04-27 23:50:05 -0400

    created our input and label fields in ResourceForm.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Resource from './components/Resource';
 import resources from './mock/resources'
 
+import ResourceForm from './components/ResourceForm';
+
 class App extends Component {
 //[<Resource resource ={aResource}/>, Resource resource ={aResource}]
 // constructor(props) {
@@ -23,8 +25,12 @@ class App extends Component {
       <div className="App">
         <div className="header">
           <h1 className="title">Welcome to BrainHive!</h1>
+          <div className="navigation">
+            <a href="/">Add Resource</a>
+          </div>
         </div>
         <div className="resourceList">{this.renderPosts()}</div>
+        <ResourceForm />
       </div>
     );
   }
