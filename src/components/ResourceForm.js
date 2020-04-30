5c1530ad90b40a20d736b1955852349a2e732449
Chang <cnxiong03@gmail.com>
2020-04-30 18:35:35 -0400

    work on some css

--- src/components/ResourceForm.js
+++ src/components/ResourceForm.js
@@ -1,79 +1,148 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 class ResourceForm extends Component {
-    state = {
-        posterName: "",
-        resourceAuthor: "",
-        cohort: "",
-        title: "",
-    };
-
-    handleChange = (e) => {
-        this.setState({
-            ...this.state,
-            [e.target.id]: e.target.value
-        })
-    }
-    render() {
-        return (
-            <div className="ResourceForm">
-                <form 
-                    style={styles.form}
-                        >
-                    {/* <label htmlFor="posterName">Your Name:</label> */}
-                    <input 
-                        style={styles.input}
-                        id="posterName" 
-                        type="text"
-                        placeholder="Your Name"
-                        value={this.state.posterName}
-                        onChange={this.handleChange} 
-                        />
-                    <input 
-                        style={styles.input}
-                        id="resourceAuthor" 
-                        type="text"
-                        placeholder="Resource Author"
-                        value={this.state.resourceAuthor}
-                        onChange={this.handleChange}  
-                        />
-                        {/* dropmenu for author level */}
-                    <input 
-                        style={styles.input}
-                        id="cohort" 
-                        type="text"
-                        placeholder="Cohort #"
-                        vale={this.state.cohort}
-                        onChange={this.handleChange}  
-                        />
-                    <input 
-                        style={styles.input}
-                        id="title" 
-                        type="text"
-                        placeholder="Resource Title"
-                        value={this.state.title}
-                        onChange={this.handleChange}  
-                        />
-                        <button type="submit">Submit</button>
-                </form>
-            </div>
-        );
-    }
-};
-
+  state = {
+    posterName: "",
+    resourceAuthor: "",
+    jobSkillLevel: "",
+    cohort: "",
+    title: "",
+    categories: "",
+    summary: "",
+    link: "",
+    resourceType: "",
+    datePublished: null,
+    videoLength: "",
+    timeToComplete: "",
+    rating: "",
+    comments: [],
+  };
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      [e.target.id]: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <div className="ResourceForm">
+        <form style={styles.form}>
+          {/* <label htmlFor="posterName">Your Name: </label> */}
+          <input
+            style={styles.input}
+            id="posterName"
+            type="text"
+            placeholder="Your Name"
+            value={this.state.posterName}
+            onChange={this.handleChange}
+          />
+          <input
+            style={styles.input}
+            id="resourceAuthor"
+            type="text"
+            placeholder="Resource Author"
+            value={this.state.resourceAuthor}
+            onChange={this.handleChange}
+          />
+          <div>
+            <select>
+              <option value="" disabled>
+                Author skill level
+              </option>
+              <option value="Intro">Intro</option>
+              <option value="Intermediate">Intermediate</option>
+              <option value="Advanced">Advanced</option>
+            </select>
+          </div>
+          <input
+            style={styles.input}
+            id="cohort"
+            type="text"
+            placeholder="Cohort #"
+            value={this.state.cohort}
+            onChange={this.handleChange}
+          />
+          <input
+            style={styles.input}
+            id="title"
+            type="text"
+            placeholder="Resource Title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+          <input
+            style={styles.input}
+            type="text"
+            id="categories"
+            placeholder="Categories (seperate multiples with commas)"
+            value={this.state.categories}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            style={styles.input}
+            type="text"
+            id="link"
+            placeholder="Resource Link"
+            value={this.state.link}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            style={styles.input}
+            type="text"
+            id="resourceType"
+            placeholder="Resource Type"
+            value={this.state.resourceType}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            style={styles.input}
+            type="date"
+            id="datePublished"
+            placeholder="Published Date"
+            value={this.state.datePublished}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            style={styles.input}
+            type="text"
+            id="videoLength"
+            placeholder="Length of Video (optional)"
+            value={this.state.videoLength}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            style={styles.input}
+            type="text"
+            id="timeToComplete"
+            placeholder="Time to complete resource"
+            value={this.state.timeToComplete}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <input
+            style={styles.input}
+            type="number"
+            id="rating"
+            placeholder="1 to 5 rating"
+            value={this.state.rating}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 const styles = {
-    form: {
-        display: 'flex', 
-        justifyContent: 'center',
-        alignItems: "center",
-        marginRight: "20%",
-        marginLeft: "20%",
-        flexDirection: "column",
-    },
-    input: {
-        width: "60%",
-        height: 32,
-    },
+  form: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: "20%",
+    marginLeft: "20%",
+    flexDirection: "column",
+  },
+  input: {
+    width: "60%",
+    height: 32,
+  },
 };
-
 export default ResourceForm;
